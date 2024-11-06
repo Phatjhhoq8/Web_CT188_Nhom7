926f96f06417598758fd9bf49575fe0e9132104d
Phatjhhoq8 <np368057@gmail.com>
2024-11-06 17:09:41 +0700

    something

--- sources/Data/userList.js
+++ sources/Data/userList.js
@@ -4,45 +4,14 @@
         userName: 'Phuongdepchai',
         pwd: 'Phuong1@@#',
         data: {
-            cart:['SP001', 'SP004', 'SP008'],
+            cart:[{item: "SP001",quantity: 2}],
             odersPlaced: ['SP002','SP025','SP021'],
             oderPaid: ['SP023','SP005','SP010'],
-            NotificationRecieved: ['SP014','SP020','SP006']
+            NotificationRecieved: ['SP014','SP020','SP006'],
+            historySearch: []
         }
     },
-    {
-        fullName: 'Tran Gia Khang',
-        userName: 'KhangTranG',
-        pwd: 'KhangBeo9!@',
-        data: {
-            cart:['SP015', 'SP026', 'SP012'],
-            odersPlaced: ['SP021','SP011','SP005'],
-            oderPaid: ['SP002','SP010','SP025'],
-            NotificationRecieved: ['SP009','SP014','SP029']
-        }
-    },
-    {
-        fullName: 'Nguyen Hai Danng',
-        userName: 'HaidangNg',
-        pwd: 'Dangngyn09@',
-        data: {
-            cart:['SP021','SP011','SP023'],
-            odersPlaced: ['SP022','SP001','SP013'],
-            oderPaid: ['SP009','SP010','SP025'],
-            NotificationRecieved: ['SP014','SP017','SP020']
-        }
-    },
-    {
-        fullName: 'Le Nguyen Anh Duy',
-        userName: 'DuyleNg',
-        pwd: 'Anhduy2711@',
-        data: {
-            cart:['SP006','SP021','SP009'],
-            odersPlaced: ['SP024','SP001','SP014'],
-            oderPaid: ['SP011','SP026','SP010'],
-            NotificationRecieved: ['SP002','SP005','SP017']
-        }
-    },
+   
 ] 
 
     
